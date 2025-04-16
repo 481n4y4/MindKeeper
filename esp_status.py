5f592f9b4258cddd8fa612a78c83aa427bfe37da
Abinaya <141475809+481n4y4@users.noreply.github.com>
2025-04-16 16:56:11 +0700

    hapus cam

--- esp_status.py
+++
@@ -1,8 +0,0 @@
-import serial.tools.list_ports
-
-def is_esp_connected():
-    ports = serial.tools.list_ports.comports()
-    for port in ports:
-        if "USB" in port.description or "CP210" in port.description or "CH340" in port.description:
-            return True
-    return False

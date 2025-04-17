1b8d1ece7ea68c810661c19770c24092929f0018
Abinaya <141475809+481n4y4@users.noreply.github.com>
2025-04-17 20:33:56 +0700

    perbaikan cam lagi

--- app.py
+++ app.py
@@ -55,8 +55,8 @@ if st.session_state.current_chat_id not in st.session_state.chat_sessions:
 
 # Sidebar: Riwayat & tombol buat chat baru + edit/hapus
 with st.sidebar:
-    st.markdown("### 📷 Cam")
-    st.link_button("Go to Camera", "http://192.168.61.99");
+    st.markdown("## 📷 Kamera")
+    st.markdown("[Buka Kamera (localhost)](http://localhost:5000)", unsafe_allow_html=True)
 
     st.markdown("## ✏️ Obrolan")
     if st.button("➕ Obrolan Baru"):
